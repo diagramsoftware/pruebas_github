e8e8e894212330256c26341cd5b458c04f4415e3
docker-odoo <odoo@d569a9dfcd5f>
2018-02-07 15:30:34 +0000

    Nueva master: 11.0.0.3 -> 11.0.1.0

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Odoo pruebas",
-    "version": "8.0.3.0",
+    "version": "11.0.1.0",
     "author": "Diagram Software, S.L.",
     "category": "NubeaERP",
     "summary": "odoo_pruebas",
