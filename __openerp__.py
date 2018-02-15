5da5285341dac30a79242b774e34f5e2d6c340b2
docker-odoo <odoo@18cff0faa1d3>
2018-02-15 09:22:53 +0000

    Nueva develop: 8.0.3.1 -> 8.0.3.2

--- __openerp__.py
+++ __openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Odoo pruebas",
-    "version": "8.0.3.1",
+    "version": "8.0.3.2",
     "author": "Diagram Software, S.L.",
     "category": "NubeaERP",
     "summary": "odoo_pruebas",
